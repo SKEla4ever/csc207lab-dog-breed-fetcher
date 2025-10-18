e735ec30f4d60afc47b36f08d0a3b1ebcbb53c46
SKEla4ever <norriswang99@gmail.com>
2025-10-17 20:58:53 -0400

    task1,2,3,4 finishd

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -15,14 +15,24 @@ import java.util.*;
 public class CachingBreedFetcher implements BreedFetcher {
     // TODO Task 2: Complete this class
     private int callsMade = 0;
+    BreedFetcher breedFetcher;
+    private final Map<String, List<String>> cache = new HashMap<>();
     public CachingBreedFetcher(BreedFetcher fetcher) {
-
+        this.breedFetcher = fetcher;
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        List<String> cached = cache.get(breed);
+        if (cached != null) {
+            return new ArrayList<>(cached);
+        }
+        callsMade++;
+        List<String> result = breedFetcher.getSubBreeds(breed);
+        cache.put(breed, new ArrayList<>(result));
+
+        return new ArrayList<>(result);
     }
 
     public int getCallsMade() {
